785cc76b3acbf36a05aa7eb9c1d73c3bedebbe01
nytec <nytec@10.8.56.222>
2024-11-04 17:41:39 +0000

    Merging repeated product items into a single request

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -9,9 +9,25 @@ import java.util.Collections;
  * @author  Your Name 
  * @version 1.0
  */
-public class BetterBasket extends Basket implements Serializable
+public class BetterBasket extends Basket
 {
-  private static final long serialVersionUID = 1L;
+  //private static final long serialVersionUID = 1L;
+	
+	@Override
+	  public boolean add( Product pr )
+	  {   
+		for (Product prInList: this) {
+			if (prInList.getProductNum().equals(pr.getProductNum())) {
+				int quantity = pr.getQuantity()+prInList.getQuantity();
+				prInList.setQuantity(quantity);
+				return (true);
+			}
+		}
+		
+	    return super.add( pr );     // Call add in ArrayList
+	  }
+	
+	
   
 
   // You need to add code here
